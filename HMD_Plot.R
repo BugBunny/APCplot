77b9e2d25d1afc6c156e56f484472253e90c16a0
Ian Timaeus <10499045+BugBunny@users.noreply.github.com>
2024-02-16 20:22:27 +0000

    Code with everything inside functions

--- HMD_Plot.R
+++ HMD_Plot.R
@@ -1,66 +1,72 @@
-# Plot APC rates properly onto equilateral rather than right-angled triangles.
+# Plot APC rates properly, using equilateral rather than right-angled triangles.
 #
-library(HMDHFDplus)
-# Download mortality data from the Human Mortality Database (HMD) at
-# https://mortality.org Note that users are required to pre-register on the site
-# in order to download the data (Note also that, if you registered before June
-# 2022, you need to re-register).
-user <- "ian.timaeus@lshtm.ac.uk"
-pw <- "178Herne!"
-# This is the country code for the total population of England and Wales. Other
-# country codes are specified. Note that not all countries have a century long
-# run of data and the plotting function has not yet been generalised for use on
-# shorter or longer data series. 
-country_id <- "GBRTENW"
-Base_year <- 1922L
-len <- 100L
-# Import the deaths and exposure counts for the lexis triangles
-Deaths_lexis <- readHMDweb(country_id, "Deaths_lexis", user, pw)
-Exposures_lexis <- readHMDweb(country_id, "Exposures_lexis", user, pw)
-# Calculate the APC death rates
-Rates_lexis <- Deaths_lexis[ , 1:3]
-Rates_lexis <- cbind(Rates_lexis, Deaths_lexis$Female / Exposures_lexis$Female)
-colnames(Rates_lexis)[ncol(Rates_lexis)] <- "Female"
-Rates_lexis <- cbind(Rates_lexis, Deaths_lexis$Male / Exposures_lexis$Male)
-colnames(Rates_lexis)[ncol(Rates_lexis)] <- "Male"
-# Convert APC measures to ranges 0:99, reversing the period axis
-Rates_lexis$age <- as.integer(Rates_lexis$Age)
-Rates_lexis$coh <- as.integer(Rates_lexis$Coh) - Base_year
-Rates_lexis$per <- as.integer(Base_year+len-1 - Rates_lexis$Year)
-Rates_lexis <- na.omit(Rates_lexis)
-Rates_lexis <- Rates_lexis[Rates_lexis$age>=0 & Rates_lexis$age<len, ]
-Rates_lexis <- Rates_lexis[Rates_lexis$coh>=0 & Rates_lexis$coh<len, ]
-Rates_lexis <- Rates_lexis[Rates_lexis$per>=0 & Rates_lexis$per<len, ]
+
+# setupHMDdata: Function to download and reorganise the mortality data from the 
+# Human Mortality Database (HMD) at https://mortality.org. Note that users must 
+# first register on the sitein order to download the data (Note also that, if 
+# you registered before June 2022, you need to re-register).
+setupHMDdata <- function(user,
+                         password,
+                         country_id = "GBRTENW",
+                         base_year = 1922L,
+                         length_yrs = 100L
+                         ) {
+   # GBRTENW is the country id for the total population of England and Wales. 
+   # Other country codes are listed in the data section of the website. Note
+   # that  not all countries have a century-long run of data and the plotting
+   # function has not yet been generalised for use with axes of variable length
+   # (e.g. 50 years of data on ages 0 to 99).
+   library(HMDHFDplus)
+   
+   # Download the deaths and exposure counts for the lexis triangles
+   Deaths_lexis <- readHMDweb(country_id, "Deaths_lexis", user, password)
+   Exposures_lexis <- readHMDweb(country_id, "Exposures_lexis", user, password)
+   
+   # Calculate the APC death rates
+   Rates_lexis <- Deaths_lexis[ , 1:3]
+   Rates_lexis <- cbind(Rates_lexis, Deaths_lexis$Female / 
+                           Exposures_lexis$Female)
+   colnames(Rates_lexis)[ncol(Rates_lexis)] <- "Female"
+   Rates_lexis <- cbind(Rates_lexis, Deaths_lexis$Male / Exposures_lexis$Male)
+   colnames(Rates_lexis)[ncol(Rates_lexis)] <- "Male"
+   Rates_lexis <- cbind(Rates_lexis, (Deaths_lexis$Male + Deaths_lexis$Female) /
+                           (Exposures_lexis$Male + Exposures_lexis$Female))
+   colnames(Rates_lexis)[ncol(Rates_lexis)] <- "Rates"
+   
+   # Compute APC indices that run from 0 upward, reversing the period axis
+   Rates_lexis$age <- as.integer(Rates_lexis$Age)
+   Rates_lexis$coh <- as.integer(Rates_lexis$Coh) - base_year
+   Rates_lexis$per <- as.integer(base_year+length_yrs-1 - Rates_lexis$Year)
+   Rates_lexis <- Rates_lexis[Rates_lexis$age>=0 & Rates_lexis$age<length_yrs, ]
+   Rates_lexis <- Rates_lexis[Rates_lexis$coh>=0 & Rates_lexis$coh<length_yrs, ]
+   Rates_lexis <- Rates_lexis[Rates_lexis$per>=0 & Rates_lexis$per<length_yrs, ]
+   Rates_lexis <- na.omit(Rates_lexis)
+   return(Rates_lexis)
+}  # End of function setupHMDdata
 #
 # APCplot: Function that tortures Martin Smith's Ternary package 
 # https://github.com/ms609/Ternary/ into drawing a pair of age-period-cohort 
-# plots of sets of rates for men and women respectively. It can plot either 
-# the individual APC rates or a contour plot.
+# plots of sets of rates for men and women respectively. It can either plot 
+# the individual APC rates triangle by triangle or a contour plot.
 # 
-# As input, APCplot expects a data frame with 10,000 observations. This should
-# include columns named age, per and coh that cover the universe of possible 
-# combinations of values of the coordinates, each ranging from 0 to 99. It  
-# should also include columns named Male and Female that contain the rates for
-# each possible combination of age, per and coh. 
+# As input, APCplot expects a data frame that should include columns named age, 
+# per and coh that cover the universe of possible combinations of values of the 
+# coordinates (so ranging from 0 to 99 in the case of mortality data). It  
+# should also include either columns named Male and Female, or a column named
+# Rates, containing the rates for each possible combination of age, per and coh.
+# Th data frame should contain  N observations, where N is the square of the 
+# length of the age, period, cohort vectors (so, for mortality, the dataset 
+# should have 10,000 observations).  
 #
-# Author: Ian Timaeus, 15 Feb 2024. https://github.com/BugBunny/APCplot
+# Author: Ian Timaeus, 16 Feb 2024. https://github.com/BugBunny/APCplot
 #
 APCplot <- function(Rates_lexis,
-                    base_yr = 0L,
+                    base_year = 0L,
                     length_yrs = 100L,
-                    contour_plot = FALSE) {
+                    contour_plot = FALSE,
+                    sex_specific = TRUE) {
    require("Ternary")
-   # Match the spectrum of colours to the range of the rates for each sex so
-   # that the same rates get coloured identically on both plots
-   minm <- min(Rates_lexis$Male)
-   minf <- min(Rates_lexis$Female)
-   maxm <- max(Rates_lexis$Male)
-   maxf <- max(Rates_lexis$Female)
-   m_end <- 1 - minm /(maxm-minf)
-   m_n <- round(256 * m_end)
-   f_start  <- 1 - maxf /(maxm-minf)
-   f_n <- round(256 * (1 - f_start))
-   # Store the rates for a specific sex in the format expected by Ternary
+   # Store the rates for a single plot in the format expected by Ternary
    DataFunction <- function(a, b, c) {
       a <- round(a * (length_yrs-1))
       b <- round(b *  (length_yrs-1) + 0.1)
@@ -70,8 +76,8 @@ APCplot <- function(Rates_lexis,
       for (i in 1:n_triangles) {
          work1 <- with(Rates_lexis, Rates_lexis[[Sex]][age==a[i] & coh==b[i] & 
                                                           per==c[i]])
-         # Fill in dummy data points atthe  end of rows that Ternary uses to 
-         # avoid RHS white space after mapping on to Cartesian coordinates
+         # Fill in the dummy data points at the end of rows that Ternary uses 
+         # to avoid RHS white space after mapping on to Cartesian coordinates
          if (length(work1) != 0) {
             work2[i] <- work1
          } else {
@@ -79,15 +85,38 @@ APCplot <- function(Rates_lexis,
          }
       }
       # Plot log rates
-      log(work2)
-   } 
+      return(log(work2))
+   }  # End of function DataFunction
+   
    # The period axis runs in reverse order to that on a true ternary plot
    alabels <- list(seq(0, length_yrs, by = length_yrs/10),
-      seq(base_yr, base_yr+length_yrs, by = length_yrs/10),
-      seq(base_yr+length_yrs, base_yr, by = -length_yrs/10))
-   # Draw the plots for the two sexes side by side
-   par(mfrow = c(1, 2), mar = rep(0.3, 4))
-   for(Sex in c("Male", "Female")) {
+      seq(base_year, base_year + length_yrs, by = length_yrs/10),
+      seq(base_year + length_yrs, base_year, by = -length_yrs/10))
+   par(mar = rep(0.8, 4))
+   
+   if (sex_specific) {
+      # Draw plots for the two sexes side by side   
+      par(mfrow = c(1, 2))
+      # Match the spectrum of colours to the range of the rates for each sex
+      # so that the same rates get coloured identically on both plots
+      minm <- min(Rates_lexis$Male)
+      minf <- min(Rates_lexis$Female)
+      maxm <- max(Rates_lexis$Male)
+      maxf <- max(Rates_lexis$Female)
+      m_end <- 1 - minm /(maxm-minf)
+      m_n <- round(256L * m_end)
+      f_start  <- 1 - maxf /(maxm-minf)
+      f_n <- round(256L * (1 - f_start))     
+      list_of_cols <- c("Male", "Female")
+   } else {
+      minf <- 0.0000454
+      maxm <- 1
+      f_start <- 0
+      f_n <- 256L
+      list_of_cols <- "Rates"
+   }
+   
+   for(Sex in list_of_cols) {
       # The two sexes use overlapping subsets of the full colour palette
       if (Sex=="Male") {
          spec = viridisLite::viridis(m_n, alpha = 0.6,
@@ -96,20 +125,20 @@ APCplot <- function(Rates_lexis,
          spec = viridisLite::viridis(f_n, alpha = 0.6,
                                      begin = f_start, end = 1, direction = -1)
       }
-      # Plot the grid for thie current sex   
+      # Plot the grid for the current sex   
       TernaryPlot(alab="Age", blab="Cohort", clab="Period",
                   axis.labels=alabels, main=Sex)
       # Plot either the individual APC rates or a smoothed contour plot
       if (!contour_plot) {
-         values <- TernaryPointValues(DataFunction, resolution = len)
+         values <- TernaryPointValues(DataFunction, resolution = length_yrs)
          ColourTernary(values, spectrum = spec)
       } else {   
-         values <- TernaryContour(DataFunction, resolution = len, filled = TRUE,
-            color.palette = function(n) viridisLite::viridis(n, alpha = 0.6,
-                                                             direction = -1)                     )
+         values <- TernaryContour(DataFunction, resolution = length_yrs, 
+            filled = TRUE, color.palette = function(n) viridisLite::viridis(n, 
+            alpha = 0.6, direction = -1)                     )
       }
       # Add legend to the RHS plot showing the full spectrum used for both sexes
-      if (Sex=="Female") {
+      if (Sex != "Male") {
          PlotTools::SpectrumLegend(
             "topright",
             legend = signif(seq(log(maxm), log(minf), length.out = 4), 2),
@@ -118,9 +147,12 @@ APCplot <- function(Rates_lexis,
             inset = 0.02,
             xpd = NA      # Do not clip at edge of figure
           )  
-      }   
-   }
-}
-# Function calls to illustrate the tw type of plot
-APCplot(Rates_lexis, base_yr = Base_year)
-APCplot(Rates_lexis, base_yr = Base_year, contour_plot = TRUE)
+      }
+   par(mfrow = c(1, 1))
+   invisible(NULL)
+}  # End of function APCplot
+
+# Function calls to illustrate the two types of plot
+Rates_lexis <- setupHMDdata(user = "", password = "")
+APCplot(Rates_lexis, base_year = 1922)
+APCplot(Rates_lexis, base_year = 1922, contour_plot = T, sex_specific = F)
